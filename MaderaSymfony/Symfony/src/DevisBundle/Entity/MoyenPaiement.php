d6b4824a701fdbd1d785831a679d98cdd14a06cf
Alison Rarchaert <alison532@live.fr>
2018-02-02 15:05:29 +0100

    Creation entity paiement

---
+++ MaderaSymfony/Symfony/src/DevisBundle/Entity/MoyenPaiement.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace DevisBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * MoyenPaiement
+ *
+ * @ORM\Table(name="moyen_paiement")
+ * @ORM\Entity(repositoryClass="DevisBundle\Repository\MoyenPaiementRepository")
+ */
+class MoyenPaiement
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Nom", type="string", length=255)
+     */
+    private $nom;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom.
+     *
+     * @param string $nom
+     *
+     * @return MoyenPaiement
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom.
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+}
