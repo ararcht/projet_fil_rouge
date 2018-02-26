7ac7eae3e49a7737a523ea9a2cb1db1261e4ea70
PiR00 <pierre.1.2@live.fr>
2018-02-26 14:27:51 +0100

    Ajout fk Devis

--- MaderaSymfony/Symfony/src/DevisBundle/Controller/DefaultController.php
+++ MaderaSymfony/Symfony/src/DevisBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace DevisBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use DevisBundle\Entity\Modele;
+use DevisBundle\Entity\Gamme;
 
 class DefaultController extends Controller
 {
@@ -27,5 +28,14 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
         $em->persist($m);
         $em->flush();
+    }
+
+    public function viewModele(){
+        $repoModele = $this->getDoctrine()->getRepository(Modele::class);
+        $repoGamme = $this->getDoctrine()->getRepository(Gamme::class);
+        $modeles = $repoModele->findAll();
+        $gammes = $repoGamme->findAll();
+
+        
     }
 }
--- MaderaSymfony/Symfony/src/DevisBundle/Entity/Devis.php
+++ MaderaSymfony/Symfony/src/DevisBundle/Entity/Devis.php
@@ -35,6 +35,40 @@ class Devis
      */
     private $dateDevis;
 
+    /**
+     * @var int
+     * @ORM\Column(name="Commande", type="integer")
+     * @ORM\OneToMany(targetEntity="DevisBundle\Entity\Commande", mappedBy="Devis")
+     */
+    private $fk_commande;
+
+    /**
+     * @var int
+     * @ORM\Column(name="Utilisateur", type="integer")
+     * ORM\OneToOne(targetEntity="DevisBundle\Entity\Utilisateur", cascade={"persist"})
+     */
+    private $fk_utilisateur;
+
+    /**
+     * @var int
+     * @ORM\Column(name="Etat", type="integer")
+     * ORM\OneToOne(targetEntity="DevisBundle\Entity\Etat", cascade={"persist"})
+     */
+    private $fk_etat;
+
+    /**
+     * @var int
+     * @ORM\Column(name="Modele", type="integer")
+     * ORM\OneToOne(targetEntity="DevisBundle\Entity\Modele", cascade={"persist"})
+     */
+    private $fk_modele;
+
+    /**
+     * @var int
+     * @ORM\Column(name="Module", type="integer")
+     * @ORM\OneToMany(targetEntity="DevisBundle\Entity\Module", mappedBy="Devis")
+     */
+    private $fk_Module;
 
     /**
      * Get id.
@@ -92,5 +126,120 @@ class Devis
     public function getDateDevis()
     {
         return $this->dateDevis;
+    }
+
+    /**
+     * Get id fk Commande.
+     *
+     * @return int
+     */
+    public function getFkCommande()
+    {
+        return $this->fk_commande;
+    }
+
+    /**
+     * Set id fk commande.
+     *
+     * @param int $id
+     *
+     * @return Commande
+     */
+    public function setFkCommande($id)
+    {
+        $this->fk_commande = $id;
+        return $this;
+    }
+
+     /**
+     * Get id fk Module.
+     *
+     * @return int
+     */
+    public function getFkModule()
+    {
+        return $this->fk_Module;
+    }
+
+    /**
+     * Set id fk Module.
+     *
+     * @param int $id
+     *
+     * @return Module
+     */
+    public function setFkModule($id)
+    {
+        $this->fk_Module = $id;
+        return $this;
+    }
+
+    /**
+     * Get id fk Modele.
+     *
+     * @return int
+     */
+    public function getFkModel()
+    {
+        return $this->fk_modele;
+    }
+
+    /**
+     * Set id fk Modele.
+     *
+     * @param int $id
+     *
+     * @return Modele
+     */
+    public function setFkModele($id)
+    {
+        $this->fk_modele = $id;
+        return $this;
+    }
+    
+    /**
+     * Get id fk Etat.
+     *
+     * @return int
+     */
+    public function getFkEtat()
+    {
+        return $this->fk_etat;
+    }
+
+    /**
+     * Set id fk Etat.
+     *
+     * @param int $id
+     *
+     * @return Etat
+     */
+    public function setFkEtat($id)
+    {
+        $this->fk_etat = $id;
+        return $this;
+    }
+
+    /**
+     * Get id fk Utilisateur.
+     *
+     * @return int
+     */
+    public function getFkUtilisateur()
+    {
+        return $this->fk_utilisateur;
+    }
+
+    /**
+     * Set id fk Utilisateur.
+     *
+     * @param int $id
+     *
+     * @return Etat
+     */
+    public function setFkUtilisateur($id)
+    {
+        $this->fk_utilisateur = $id;
+        return $this;
     }
 }
