eb82f3f77e28d0d88bb124b122eab696a5f10b73
ThomasL <test.test@zedfe.com>
2018-02-26 14:59:28 +0100

    Revert "bdd"
    This reverts commit 0755494ec5ea91a322bd93aa39b0c2c4c2f7651f.

--- MaderaSymfony/Symfony/src/DevisBundle/Entity/Adresse.php
+++ MaderaSymfony/Symfony/src/DevisBundle/Entity/Adresse.php
@@ -12,24 +12,6 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Adresse
 {
-
-    /**
-     * @ORM\Column(name="Utilisateur",type="integer")
-     * @ORM\ManyToOne(targetEntity="DevisBundle\Entity\Utilisateur")
-     */
-     private $fk_utilisateur;
-
-     public function setUtilisateur(UserType $fk_utilisateur)
-     {
-       $this->fk_utilisateur = $fk_utilisateur;
-       return $this;
-     }
-
-     public function getUtilisateur()
-     {
-       return $this->fk_utilisateur;
-     }
-
     /**
      * @var int
      *
