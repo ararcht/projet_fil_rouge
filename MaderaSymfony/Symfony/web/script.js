ff2418b88d7fe49a1fc2cc2bb43fc57b552b5fe2
PiR00 <pierre.1.2@live.fr>
2018-03-30 13:17:13 +0200

    Merge jul

--- MaderaSymfony/Symfony/web/script.js
+++ MaderaSymfony/Symfony/web/script.js
@@ -9,53 +9,32 @@ $(".modulePanel").on('click', function(){
     }
 });
 
-
-$(".selectUnite").each( function(){
-    var id = $(this).attr("id");
-    var unit = $("#"+id).find(":selected").text();
-    var idPrix = "prixCompo"+id.split("Compo")[1];
-    var prix = $("#"+idPrix).text();
-    var price = prix * unit;
-    total = $("#prix").text();
-    var p1 = parseInt(price);
-    var p2 = parseInt(total);
-    total = p1 + p2;
-    $("#prix").text(total);
-    getSelectedOption();
-});
-
-<<<<<<< HEAD
-
+function calculPrice(){
+  var total = 100000;
+    $(".selectUnite").each( function(){
+        var id = $(this).attr("id");
+        var unit = $("#"+id).find(":selected").text();
+        var idPrix = "prixCompo"+id.split("Compo")[1];
+        var prix = $("#"+idPrix).text();
+        var price = prix * unit;
+        var p1 = parseInt(price);
+        var p2 = parseInt(total);
+        total = p1 + p2;
+        $("#prix").text(total);
+    });
+}
 
 $(".selectUnite").change(function() {
-  var total = 50000;
-  $(".selectUnite").each( function(){
-      var id = $(this).attr("id");
-      var unit = $("#"+id).find(":selected").text();
-      var idPrix = "prixCompo"+id.split("Compo")[1];
-      var prix = $("#"+idPrix).text();
-      var price = prix * unit;
-      var p1 = parseInt(price);
-      var p2 = parseInt(total);
-      total = p1 + p2;
-      $("#prix").text(total);
-  });
+  calculPrice();
+  getSelectedOption();
 });
-=======
->>>>>>> d1ff687d317fad7be11205e939a3ec7272e4ba95
+
 function getSelectedOption(){
     var count = 0;
     var listvalue = $(".selectUnite").find(":selected").text();
     for (let index = 0; index < listvalue.length; index++) {
         count = count +parseInt(listvalue[index]);
-<<<<<<< HEAD
-
     }
     $("#nOpt").text(count);
 }
-=======
-        
-    }
-    $("#nOpt").text(count);   
-}
->>>>>>> d1ff687d317fad7be11205e939a3ec7272e4ba95
+    
