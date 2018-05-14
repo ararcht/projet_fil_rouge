088b6179cece8c22818ef377ff49ecf5cbd38e94
ThomasL <test.test@zedfe.com>
2018-05-14 11:23:48 +0200

    Add 3d module

---
+++ MaderaSymfony/Symfony/web/3d_house/js/Init.js
@@ -0,0 +1,54 @@
+// Page entièrement chargé, on lance le jeu
+document.addEventListener("DOMContentLoaded", function () {
+    new Init('renderCanvas');
+}, false);
+
+Init = function(canvasId) {
+    // Canvas et engine défini ici
+    var canvas = document.getElementById(canvasId);
+    var engine = new BABYLON.Engine(canvas, true);
+    var _this = this;
+
+	// On initie la scène avec une fonction associé à l'objet Init
+
+	this.scene = this._initScene(engine);
+
+	// On apelle Camera
+	var _camera = new Camera(_this, canvas);
+
+	// On apelle House_1
+	var _house_1 = new House_1(_this);
+
+	//Début chargement
+	engine.displayLoadingUI();
+	// Permet au jeu de tourner
+	engine.runRenderLoop(function () {
+		_this.scene.render();
+		//Fin Chargement
+		engine.hideLoadingUI();
+	});
+
+    // Ajuste la vue 3D si la fenetre est agrandi ou diminué
+    window.addEventListener("resize", function () {
+        if (engine) {
+            engine.resize();
+        }
+    },false);
+
+};
+
+Init.prototype = {
+    // Prototype d'initialisation de la scène
+    _initScene : function(engine) {
+        var scene = new BABYLON.Scene(engine);
+        scene.clearColor=new BABYLON.Color3(0.9,0.9,0.9);
+        return scene;
+    }
+};
+
+//Si WebGL non supporté por le navigateur affichage d'une erreur
+if (!BABYLON.Engine.isSupported()) {
+    document.getElementById("notSupported").className = "";
+} else {
+    //loadCustomScene(demo.constructor, demo.onload);
+};
